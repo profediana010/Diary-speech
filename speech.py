21999ff49f992d516dbe78abbe555884aee1c1de
profediana010 <diana.serrano.kodland@gmail.com>
2025-07-01 15:48:21 -0500

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,11 @@
+import speech_recognition as speech_recog
+#Debes tener instaladas SpeechRecognition y pyaudio
+def speech():
+    mic = speech_recog.Microphone()
+    recog = speech_recog.Recognizer()
+
+       
+        
+        
+
+    
